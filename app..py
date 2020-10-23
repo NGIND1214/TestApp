c9a6ba589803f5291c3b7a1a225f2801d56c8ae7
Deepa <dyg.tech04@gmail.com>
2020-10-23 15:53:23 +0530

    First Commit

---
+++ app..py
@@ -0,0 +1,9 @@
+import math
+#Math Function
+
+c = 5.5
+print(round(c))
+print(abs(-3.5))
+print(math.floor(33.4))
+l = math.comb(5, 20)
+print(l)
---
+++ com.py
@@ -0,0 +1,48 @@
+
+print('Deepa Gaikwad...')
+print('o----')
+print(' ||||')
+print('*' * 10)
+price = 110
+rating = 4.5
+name = 'Deepa'
+is_published = False
+
+
+# full_name = 'John Smith'
+# age = 20
+# is_new = True
+# name = input('What is your name ? ')
+# color = input('Your favourite Color ? ')
+# print('Hi '+ name + ' your favourite color is '+ color)
+
+birth_year = input('Birth Year ')
+age = 2020 - int(birth_year)
+print(age)
+
+email = '''
+Hi John,
+How are you doing.
+
+Thanks
+'''
+print(email)
+course = "Python course"
+print(course[0])
+print(course[0:4])
+print(course[1:])
+print(course[:5])
+print(course[:])
+print(course[1:-1])
+print('Addition' + 10+3)
+print('Substraction' + 15-2)
+print('Multiplication' + 10*2)
+print('Division - 1' + 10/6)
+print('Division -2' + 10//6)
+print('Division - 3' + 10%6)
+print('Exponential/Power'+ 10**3)
+y = 10
+y += 3
+print('Augumented operator', y)
+c = 3 + 4 ** 2 / 2
+print('' + c)
